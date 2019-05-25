1619562e9c7689af4dbbef5d9df2251ac4ad0c67
Michael Chilaka <chilakamichael001@gmail.com>
2019-05-25 16:34:35 +0100

    ConfirmEmailMessage.js, DashboardPage.js, UserRoute.js, GuestRoute.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 import DashboardPage from './components/pages/DashboardPage';
+import UserRoute from './components/routes/UserRoute';
+import GuestRoute from './components/routes/GuestRoute';
 
-const App = () => (
+
+const App = ({ location }) => (
   <div className="ui container">
-    <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
-    <Route path="/dashboard" exact component={DashboardPage} />
+    <Route location={location} path="/" exact component={HomePage} />
+    <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
   </div>
 );
+
+App.propTypes = {
+  
+}
 
 export default App;
